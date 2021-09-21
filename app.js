5ef6e528e90c68c060b3f333560936f7799a1a87
ssmyrd <s.smyrd@devesoft.pl>
2021-09-21 11:53:18 +0200

    Full test pipline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Sone sewe wita CIe '));
+app.get('/', (req, res) => res.send('LOLOLO'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
